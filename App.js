efb796435d3ce85b345e939866fe8a84a7dc93b6
Imanthi Uthpala <imanthiuthpala@gmail.com>
2024-04-30 03:11:54 +0530

    dependencies for navigation

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {Home} from "./scr/app/Home";
+import 'react-native-gesture-handler';
+
+const Stack = createNativeStackNavigator();
+
+
+
+
+
+
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-      <Text>welcomePPP!</Text>
-      <StatusBar style="auto" />
-    </View>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
