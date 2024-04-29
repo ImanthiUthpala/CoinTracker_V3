4e16280679c82d1a0d745f73b2bb5da5c1e8a69b
Imanthi Uthpala <imanthiuthpala@gmail.com>
2024-04-30 01:30:16 +0530

    New confic

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
+      <View style={styles.container}>
       <Text>welcomePPP!</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaProvider>
   );
 }
 
