833caed08be092ab1b2c02ff66ed1da633f97df4
Imanthi Uthpala <imanthiuthpala@gmail.com>
2024-05-01 14:44:12 +0530

    corrected centering header align to center

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome5 } from '@expo/vector-icons';
+//import { Screen } from 'react-native-screens';
 
 import 'react-native-gesture-handler';
 
@@ -18,28 +19,53 @@ import {Tips} from "./scr/app/Tips";
 
 
 
-//const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 const TrackTab = createMaterialTopTabNavigator();
 const PlanTab = createMaterialTopTabNavigator();
 
+function StackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'My home',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+
+
+
  function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} options={{tabBarIcon:({color, size})=>(
+      <Tab.Navigator options={{headerTitleAlign:'center'}}
+       >
+      <Tab.Screen name="Home" component={Home} options={{title:'Home' ,headerTitleAlign:'center' ,tabBarIcon:({color, size})=>(
         <FontAwesome5 name="home" size={24} color="black" />
       )}} />
-      <Tab.Screen name="Track" component={Track} options={{tabBarIcon:({color, size})=>(
+      <Tab.Screen name="Track" component={Track} options={{title:'Track' ,headerTitleAlign:'center' ,tabBarIcon:({color, size})=>(
         <FontAwesome5 name="money-check" size={24} color="black" />
       )}} />
-      <Tab.Screen name="Plan" component={Plan} options={{tabBarIcon:({color, size})=>(
+      <Tab.Screen name="Plan" component={Plan} options={{title:'Plan' ,headerTitleAlign:'center' ,tabBarIcon:({color, size})=>(
         <FontAwesome5 name="pen-alt" size={24} color="black" />
       )}} />
-      <Tab.Screen name="Report" component={Report} options={{tabBarIcon:({color, size})=>(
+      <Tab.Screen name="Report" component={Report} options={{title:'Report' ,headerTitleAlign:'center' ,tabBarIcon:({color, size})=>(
         <FontAwesome5 name="chart-line" size={24} color="black" />
       )}}/>
-      <Tab.Screen name="Tips" component={Tips} options={{tabBarIcon:({color, size})=>(
+      <Tab.Screen name="Tips" component={Tips} options={{title:'Tips' ,headerTitleAlign:'center' ,tabBarIcon:({color, size})=>(
         <FontAwesome5 name="lightbulb" size={24} color="black" />      )}}/>
     </Tab.Navigator>
     </NavigationContainer>
